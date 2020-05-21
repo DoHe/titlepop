e9aae29e22df7fd4dec52194b6b4d054edc4d318
dohe <wahlholz@gmx.de>
2020-05-21 12:17:06 +0200

    Initial

---
+++ scripts/background.js
@@ -0,0 +1,27 @@
+function clicked(info, tab) {
+  const { targetElementId } = info;
+
+  browser.tabs.executeScript(tab.id, {
+    frameId: info.frameId,
+    code: `
+      if (typeof targetElementId === 'undefined') {
+        var targetElementId = ${targetElementId}; 
+      } else {
+        targetElementId = ${targetElementId}; 
+      }
+    `,
+  }, () => {
+    browser.tabs.executeScript(tab.id,
+      {
+        frameId: info.frameId,
+        file: '/scripts/pop.js',
+      });
+  });
+}
+
+browser.menus.create({
+  id: 'pop-title',
+  title: browser.i18n.getMessage('contextMenuItem'),
+  contexts: ['image'],
+  onclick: clicked,
+});
---
+++ scripts/pop.js
@@ -0,0 +1,91 @@
+function pop() {
+  function modalHtml(content) {
+    return `
+  <div class="titlepop-modal titlepop-show-modal">
+  <div class="titlepop-modal-content">
+      <span class="titlepop-close-button">&times;</span>
+      <h1>${content}</h1>
+  </div>
+  </div>
+  `;
+  }
+
+  const modalCss = `
+  .titlepop-modal {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    opacity: 0;
+    visibility: hidden;
+    transform: scale(1.1);
+    transition: visibility 0s linear 0.25s, opacity 0.25s 0s, transform 0.25s;
+  }
+  .titlepop-modal-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    padding: 1rem 1.5rem;
+    width: 24rem;
+    border-radius: 0.5rem;
+  }
+  .titlepop-close-button {
+    float: right;
+    width: 1.5rem;
+    line-height: 1.5rem;
+    text-align: center;
+    cursor: pointer;
+    border-radius: 0.25rem;
+    background-color: lightgray;
+  }
+  .titlepop-close-button:hover {
+    background-color: darkgray;
+  }
+  .titlepop-show-modal {
+    opacity: 1;
+    visibility: visible;
+    transform: scale(1.0);
+    transition: visibility 0s linear 0s, opacity 0.25s 0s, transform 0.25s;
+  }
+  `;
+
+  const image = browser.menus.getTargetElement(targetElementId);
+
+  let modal = document.querySelector('.titlepop-modal');
+  if (modal) {
+    console.log('already there');
+    modal.classList.add('titlepop-show-modal');
+  } else {
+    const cssNode = document.createElement('style');
+    cssNode.type = 'text/css';
+    cssNode.innerText = modalCss;
+    document.head.appendChild(cssNode);
+
+    const modalNode = document.createElement('div');
+    modalNode.innerHTML = modalHtml(image.title);
+    document.body.appendChild(modalNode);
+
+    modal = document.querySelector('.titlepop-modal');
+
+    const closeButton = document.querySelector('.titlepop-close-button');
+
+    function toggleModal() {
+      modal.classList.toggle('titlepop-show-modal');
+    }
+
+    function windowOnClick(event) {
+      if (event.target === modal) {
+        toggleModal();
+      }
+    }
+
+    closeButton.addEventListener('click', toggleModal);
+    window.addEventListener('click', windowOnClick);
+  }
+}
+
+pop();
